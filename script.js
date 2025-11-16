4128f0357a136b582fd031b38caa47adc56509e3
deepcode <deepakptdr2209@gmail.com>
2025-11-17 05:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+document.getElementById("button").addEventListner("click",()=>{
+	const name = getElementById("name").value.trim();
+	const year = getElementById("year").value.trim();
+	const base = "https://localhost:8080/";
+	const query = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+	document.getElementById("url").textContent = base+query;
+});
